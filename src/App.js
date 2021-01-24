fa00c433a8bc4e5be72608c5a864b2f16a4843a1
Rodrigo Bernardino <rodrigobernardino1@gmail.com>
2021-01-24 15:27:52 -0300

    Adding the solution of the first challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects')
+      .then((response) => {
+        setProjects(response.data);
+      });
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const addResponse =  await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Rodrigo Bernardino"
+    });
+
+    setProjects([...projects, addResponse.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete('projects/' + id);
+    setProjects(projects.filter(p => p.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {projects.map(project => (
+          <li key={project.id}>
+            {project.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
